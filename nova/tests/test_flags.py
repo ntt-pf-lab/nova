452de1c92bd9b4a55a79a4c501bfbb29cfa60f37
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-10-24 00:20:43 -0700

    Add testcases for nova.flags Fixes bug 869862.

--- nova/tests/test_flags.py
+++ nova/tests/test_flags.py
@@ -3,6 +3,8 @@
 # Copyright 2010 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
+# Copyright 2011 NTT
+# All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -15,19 +17,23 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+"""
+Tests For nova.flags
+"""
 
-from nova import exception
+import gflags
 from nova import flags
 from nova import test
+from nose.plugins.attrib import attr
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('flags_unittest', 'foo', 'for testing purposes only')
 
 
-class FlagsTestCase(test.TestCase):
+class FlagsValuesTestCase(test.TestCase):
 
     def setUp(self):
-        super(FlagsTestCase, self).setUp()
+        super(FlagsValuesTestCase, self).setUp()
         self.FLAGS = flags.FlagValues()
         self.global_FLAGS = flags.FLAGS
 
@@ -64,6 +70,101 @@ class FlagsTestCase(test.TestCase):
         self.assertEqual(self.FLAGS.false, True)
         self.assertEqual(self.FLAGS.true, False)
 
+    @attr(kind='small')
+    def test_call_flags_use_gnu_get_opt(self):
+        """
+        in case using GNU getopt
+        """
+        self.FLAGS.UseGnuGetOpt()
+        self.assert_('string' not in self.FLAGS)
+        self.assert_('int' not in self.FLAGS)
+        self.assert_('false' not in self.FLAGS)
+        self.assert_('true' not in self.FLAGS)
+
+        flags.DEFINE_string('string', 'default', 'desc',
+                            flag_values=self.FLAGS)
+        flags.DEFINE_integer('int', 1, 'desc', flag_values=self.FLAGS)
+        flags.DEFINE_bool('false', False, 'desc', flag_values=self.FLAGS)
+        flags.DEFINE_bool('true', True, 'desc', flag_values=self.FLAGS)
+
+        self.assert_(self.FLAGS['string'])
+        self.assert_(self.FLAGS['int'])
+        self.assert_(self.FLAGS['false'])
+        self.assert_(self.FLAGS['true'])
+        self.assertEqual(self.FLAGS.string, 'default')
+        self.assertEqual(self.FLAGS.int, 1)
+        self.assertEqual(self.FLAGS.false, False)
+        self.assertEqual(self.FLAGS.true, True)
+
+        argv = ['flags_test',
+                '--string', 'foo',
+                '--int', '2',
+                '--false',
+                '--notrue']
+
+        self.FLAGS(argv)
+        self.assertEqual(self.FLAGS.string, 'foo')
+        self.assertEqual(self.FLAGS.int, 2)
+        self.assertEqual(self.FLAGS.false, True)
+        self.assertEqual(self.FLAGS.true, False)
+
+    @attr(kind='small')
+    def test_call_exception_unrecognized_flag_error_with_unparsed_args(self):
+        """
+        exception will not be raised when __call__() is called before DECLARE
+        """
+        argv = ['flags_test', '--string', 'foo']
+        args = self.FLAGS(argv)
+        self.assertEqual(['flags_test', 'foo'], args)
+
+    @attr(kind='small')
+    def test_call_exception_unrecognized_flag_error_with_unparsed_args_use_gnu_get_opt(self):
+        """
+        exception will not be raised when __call__() is called before DECLARE in using GNU getopt
+        """
+        self.FLAGS.UseGnuGetOpt()
+        argv = ['flags_test', '--string', 'foo']
+        args = self.FLAGS(argv)
+        self.assertEqual(['flags_test', 'foo'], args)
+
+    @attr(kind='small')
+    def test_call_exception_unrecognized_flag_error_without_unparsed_args(self):
+        """
+        exception will not be raised when argument of the option is invalid in using GNU getopt
+        """
+        self.FLAGS.UseGnuGetOpt()
+        argv = ['flags_test', '--string']
+        args = self.FLAGS(argv)
+        self.assertEqual(['flags_test'], args)
+
+    @attr(kind='small')
+    def test_getitem_configuration_flag_is_dirty(self):
+        """
+        ParseNewFlags() is executed before __getitem__() is called
+        """
+        argv = ['flags_test', '--string', 'foo']
+        self.FLAGS(argv)
+        self.assertFalse(self.FLAGS.IsDirty('string'))
+        
+        flags.DEFINE_string('string', 'bar', 'desc',
+                            flag_values=self.FLAGS)
+        self.assert_(self.FLAGS.IsDirty('string'))
+        self.assert_(self.FLAGS['string'])
+
+    @attr(kind='small')
+    def test_getattr_configuration_flag_is_dirty(self):
+        """
+        ParseNewFlags() is executed before __getattr__() is called
+        """
+        argv = ['flags_test', '--string', 'foo']
+        self.FLAGS(argv)
+        self.assertFalse(self.FLAGS.IsDirty('string'))
+        
+        flags.DEFINE_string('string', 'bar', 'desc',
+                            flag_values=self.FLAGS)
+        self.assert_(self.FLAGS.IsDirty('string'))
+        self.assertEqual('foo', self.FLAGS.string)
+
     def test_declare(self):
         self.assert_('answer' not in self.global_FLAGS)
         flags.DECLARE('answer', 'nova.tests.declare_flags')
@@ -75,6 +176,15 @@ class FlagsTestCase(test.TestCase):
         self.assertEqual(self.global_FLAGS.answer, 256)
         flags.DECLARE('answer', 'nova.tests.declare_flags')
         self.assertEqual(self.global_FLAGS.answer, 256)
+
+    @attr(kind='small')
+    def test_declare_flags_name_not_in_flag_values(self):
+        """
+        UnrecognizedFlag is raised when name not in flag_values
+        """
+        self.assertRaises(gflags.UnrecognizedFlag,
+                          flags.DECLARE,
+                          'dummy_flag', 'nova.tests.declare_flags')
 
     def test_runtime_and_unknown_flags(self):
         self.assert_('runtime_answer' not in self.global_FLAGS)
@@ -114,3 +224,31 @@ class FlagsTestCase(test.TestCase):
         self.assertEqual(FLAGS.flags_unittest, 'foo')
         FLAGS.flags_unittest = 'bar'
         self.assertEqual(FLAGS.flags_unittest, 'bar')
+
+class StrWrapperTestCase(test.TestCase):
+
+    def setUp(self):
+        super(StrWrapperTestCase, self).setUp()
+        self.context = flags.FlagValues()
+        self.extra_context = flags.FlagValues()
+
+    @attr(kind='small')
+    def test_getitem(self):
+        flags.DEFINE_string('string', 'default', 'desc',
+                            flag_values=self.context)
+        flags.DEFINE_integer('int', 1, 'desc',
+                             flag_values=self.extra_context)
+        wrapper = flags.StrWrapper([self.context,
+                                    self.extra_context])
+        self.assertEqual('default', wrapper['string'])
+        self.assertEqual('1', wrapper['int'])
+
+    @attr(kind='small')
+    def test_getitem_exception_key_error(self):
+        """
+        KeyError is raised when val is not found
+        """
+        wrapper = flags.StrWrapper([self.context])
+        self.assertRaises(KeyError,
+                          wrapper.__getitem__,
+                          'string')
