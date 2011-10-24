183029e85fdad168dc8f2da7815f6d56baf98701
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-10-24 14:05:02 +0900

    Add testcases for nova.virt.vif.py Fixes bug 877980.

---
+++ nova/tests/test_virt_vif.py
@@ -0,0 +1,43 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011 NTT
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+"""
+Tests For nova.virt.vif
+"""
+
+from nova import test
+from nova.virt import vif
+from nose.plugins.attrib import attr
+
+
+class VIFDriverTestCase(test.TestCase):
+    """Test for nova.virt.vif.VIFDriver. """
+    def setUp(self):
+        super(VIFDriverTestCase, self).setUp()
+
+    @attr(kind='small')
+    def test_plug(self):
+        """Test for nova.virt.vif.VIFDriver.plug. """
+        self.vifdriver = vif.VIFDriver()
+        self.assertRaises(NotImplementedError, self.vifdriver.plug,
+                          'fake_instance', 'fake_network', 'fake_mapping')
+
+    @attr(kind='small')
+    def test_unplug(self):
+        """Test for nova.virt.vif.VIFDriver.unplug. """
+        self.vifdriver = vif.VIFDriver()
+        self.assertRaises(NotImplementedError, self.vifdriver.unplug,
+                          'fake_instance', 'fake_network', 'fake_mapping')
