57a8300e3866389d22fa64100910b5d177242f31
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-10-31 15:21:12 +0900

    Modify testcases for nova.compute.utils Fixes bug 868103

--- nova/tests/test_compute_utils.py
+++ nova/tests/test_compute_utils.py
@@ -33,6 +33,7 @@ block_device_mappings = [{'id': 1,
                           'volume_id': 'vol-00000002',
                           'delete_on_termination': True}]
 
+
 class UtilsTestCase(test.TestCase):
     """Test for nova.compute.utils. """
     def setUp(self):
@@ -44,50 +45,67 @@ class UtilsTestCase(test.TestCase):
     def test_terminate_volumes(self):
         """Test for nova.compute.utils.terminate_volumes. """
         self._bdm_id = None
+
         def stub_block_device_mapping_destroy(context, bdm_id):
             self._bdm_id = bdm_id
-        
-        self.mox.StubOutWithMock(db, 'block_device_mapping_get_all_by_instance')
+
+        self.mox.StubOutWithMock(db,
+                            'block_device_mapping_get_all_by_instance')
         self.stubs.Set(db, 'block_device_mapping_destroy',
                             stub_block_device_mapping_destroy)
         db.block_device_mapping_get_all_by_instance(mox.IgnoreArg(),
                                                     mox.IgnoreArg()).\
-                                            AndReturn([block_device_mappings[0]])
+                                AndReturn([block_device_mappings[0]])
         self.mox.ReplayAll()
-        
+
         self.utils.terminate_volumes(db, self.context, 1)
         self.assertEqual(block_device_mappings[0]['id'], self._bdm_id)
+
+    @attr(kind='small')
+    def test_terminate_volumes_parameter_instance_not_found(self):
+        """
+        InstanceNotFound is raised when specified instance is not found
+        """
+        self.assertRaises(exception.InstanceNotFound,
+                          self.utils.terminate_volumes,
+                          db, self.context, 99999)
 
     @attr(kind='small')
     def test_terminate_volumes_database_volume_id_is_not_none_and_delete_on_termination_is_true(self):
         """Test for nova.compute.utils.terminate_volumes. """
         self._volume_id = None
+
         def stub_delete(caller, context, volume_id):
             self._volume_id = volume_id
-        
-        self.mox.StubOutWithMock(db, 'block_device_mapping_get_all_by_instance')
+
+        self.mox.StubOutWithMock(db,
+                            'block_device_mapping_get_all_by_instance')
         self.stubs.Set(volume_api.API, 'delete', stub_delete)
         self.mox.StubOutWithMock(db, 'block_device_mapping_destroy')
         db.block_device_mapping_get_all_by_instance(mox.IgnoreArg(),
                                                     mox.IgnoreArg()).\
-                                            AndReturn([block_device_mappings[1]])
+                                AndReturn([block_device_mappings[1]])
         db.block_device_mapping_destroy(mox.IgnoreArg(), mox.IgnoreArg())
         self.mox.ReplayAll()
-        
+
         self.utils.terminate_volumes(db, self.context, 1)
-        self.assertEqual(block_device_mappings[1]['volume_id'], self._volume_id)
+        self.assertEqual(block_device_mappings[1]['volume_id'],
+                         self._volume_id)
 
     @attr(kind='small')
     def test_terminate_volumes_exception_volume_api_delete(self):
         """
-        All BlockDeviceMappings are deleted even when exception is raised in volume_api.delete()
+        All BlockDeviceMappings are deleted
+        even when exception is raised in volume_api.delete()
         """
         self._count = 0
+
         def stub_delete(caller, context, volume_id):
             self._count += 1
             raise exception.ApiError()
-        
-        self.mox.StubOutWithMock(db, 'block_device_mapping_get_all_by_instance')
+
+        self.mox.StubOutWithMock(db,
+                            'block_device_mapping_get_all_by_instance')
         self.stubs.Set(volume_api.API, 'delete', stub_delete)
         self.mox.StubOutWithMock(db, 'block_device_mapping_destroy')
         db.block_device_mapping_get_all_by_instance(mox.IgnoreArg(),
@@ -95,7 +113,7 @@ class UtilsTestCase(test.TestCase):
                                             AndReturn(block_device_mappings)
         db.block_device_mapping_destroy(mox.IgnoreArg(), mox.IgnoreArg())
         self.mox.ReplayAll()
-        
+
         self.assertRaises(exception.ApiError, self.utils.terminate_volumes,
                           db, self.context, 1)
         self.assertEqual(2, self._count)
