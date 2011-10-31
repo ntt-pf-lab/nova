6980e82e0cb33f2279a33fa5a0eca26762943475
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-10-31 18:01:26 +0900

    Add testcases for /nova/db/sqlalchemy/session Fixes bug 877981.

--- nova/tests/db/test_sqlalchemy_session.py
+++ nova/tests/db/test_sqlalchemy_session.py
@@ -26,18 +26,19 @@ import sqlalchemy
 
 FLAGS = flags.FLAGS
 
+
 class SQLAlchemySessionTestCase(test.TestCase):
 
     @attr(kind='small')
     def test_get_session(self):
         session = db_session.get_session()
-        self.assert_('sqlalchemy.orm.session.Session object'in str(session))
+        self.assert_('sqlalchemy.orm.session.Session object' in str(session))
 
     @attr(kind='small')
     def test_get_session_configuration(self):
         """ Ensure get engine, get maker and return a SQLAlchemy session"""
         db_session._ENGINE = None
-        db_session._MAKER = None        
+        db_session._MAKER = None
         session = db_session.get_session()
         self.assert_(db_session._ENGINE is not None)
         self.assert_(db_session._MAKER is not None)
@@ -45,21 +46,23 @@ class SQLAlchemySessionTestCase(test.TestCase):
 
     @attr(kind='small')
     def test_get_session_parametar(self):
-        """ Ensure return a SQLAlchemy session when parametar is not default"""
-        session = db_session.get_session(autocommit=False, expire_on_commit=True)
-        self.assert_('sqlalchemy.orm.session.Session object'in str(session))
+        """ Ensure return a SQLAlchemy session
+            when parametar is not default"""
+        session = db_session.get_session(autocommit=False,
+                                         expire_on_commit=True)
+        self.assert_('sqlalchemy.orm.session.Session object' in str(session))
 
     @attr(kind='small')
     def test_get_engine_configuration_drivername_is_other(self):
-        """ Ensure return a SQLAlchemy engine when drivername is not sqlite"""
+        """ Ensure return a SQLAlchemy engine
+            when drivername is not sqlite"""
         self.flags(sql_connection='other:///home/fake/other_db')
 
         def fake_create_engine(*cmd, **kwargs):
             return 'other.engine'
 
-        self.stubs.Set(sqlalchemy, 'create_engine', 
+        self.stubs.Set(sqlalchemy, 'create_engine',
                        fake_create_engine)
 
         engine = db_session.get_engine()
         self.assertEqual('other.engine', engine)
-
